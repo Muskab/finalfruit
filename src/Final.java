eea840e546928c2537e06a509c4b221bfff51bb4
mbabak <muskababak@ymail.com>
2020-06-19 14:15:12 -0400

    fruit

---
+++ src/Final.java
@@ -0,0 +1,18 @@
+public class Final {
+    public static void main(String [] Args){
+        String[] fruits = new String[10];
+        String[] fruties = {"Kiwi, pineapple, apple, banana, orange, grape, watermelon, cherry strawberry, melon"};
+        System.out.print("Orginal:");
+        for(int f = 0; f < (fruties.length); f++) {
+
+            System.out.println(fruties[f]);
+
+        }
+        // going to try accending order!
+
+        String[] Alphabetical = new String[10];
+        for(int f = 0; f < (fruties.length); f++);{
+
+        }
+    }
+}
