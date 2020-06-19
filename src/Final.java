3e841116b659ad79810a43a02fec83f1dbb85cf8
mbabak <muskababak@ymail.com>
2020-06-19 16:02:03 -0400

    final

--- src/Final.java
+++ src/Final.java
@@ -1,18 +1,56 @@
+import java.util.Scanner;
+
 public class Final {
     public static void main(String [] Args){
-        String[] fruits = new String[10];
-        String[] fruties = {"Kiwi, pineapple, apple, banana, orange, grape, watermelon, cherry strawberry, melon"};
-        System.out.print("Orginal:");
+        Scanner ff = new Scanner (System.in);
+        String [] fruties = new String[10];
+        System.out.println("Enter fruits [10]: ");
+        for(int f = 0; f < 10; f++){
+            fruties[f] = ff.next();
+
+            }
+
+        System.out.print("original:");
         for(int f = 0; f < (fruties.length); f++) {
 
             System.out.println(fruties[f]);
-
         }
+
         // going to try accending order!
 
-        String[] Alphabetical = new String[10];
-        for(int f = 0; f < (fruties.length); f++);{
+        String fru = "";
+        String [] fafa = new String [10];
+        for (int f = 0; f < (fruties.length -1) ; f++){
+            for (int m = 0; m < (fruties.length -f) -1; m ++ ){
+                if (fruties[m].compareToIgnoreCase(fruties[m+1]) > 0) {
+                    fru = fruties[m];
+                    fruties[m] = fruties[m + 1];
+                    fruties[m + 1] = fru;
+                }
 
+
+
+                }
+            }
+        System.out.println("accending order");
+        for(int f = 0; f < (fruties.length); f ++){
+            System.out.println(fruties[f]);
         }
+
+        // going to try Decending order!
+
+        System.out.println("Decending order");
+        for(int f = fruties.length -1; f >= 0; f--){
+            System.out.println(fruties [f]);
+        }
+        }
+
+
     }
-}
+
+
+
+
+
+
+
